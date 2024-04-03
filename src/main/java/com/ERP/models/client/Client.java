012d71b9375242fe6cd7744ba7878f6fd531c3a9
felipe <felipestylex08@gmail.com>
2024-04-02 21:40:43 -0300

    create client

---
+++ src/main/java/com/ERP/models/client/Client.java
@@ -0,0 +1,24 @@
+package com.ERP.models.client;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="clients")
+@Table(name="clients")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String adress;
+    private String telephone;
+    private String email;
+}
