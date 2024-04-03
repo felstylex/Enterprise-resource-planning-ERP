f624caf247d51f4c4c308e585b763918d5addb55
felipe <felipestylex08@gmail.com>
2024-04-03 19:25:36 -0300

    create product

---
+++ src/main/java/com/ERP/models/products/Product.java
@@ -0,0 +1,27 @@
+package com.ERP.models.products;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name="products")
+@Table(name="products")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int stock;
+    private String supplier_id;
+}
