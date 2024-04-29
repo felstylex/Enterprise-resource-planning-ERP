2d8d6fc88ba9126638b81ee7add7fd71f1c0da14
felipe <felipestylex08@gmail.com>
2024-04-29 15:04:11 -0300

    small modifications

--- src/main/java/com/ERP/services/CalculationService.java
+++ src/main/java/com/ERP/services/CalculationService.java
@@ -1,12 +1,9 @@
 package com.ERP.services;
 
 import com.ERP.interfaces.Order;
-import com.ERP.models.salesOrder.SalesOrder;
-import com.ERP.models.salesOrder.SalesOrderItem;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Service
 public class CalculationService {
